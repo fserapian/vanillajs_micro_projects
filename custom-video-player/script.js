5e9544bbbb4026c7d2c630f0549a5afea374864a
Fadi Serapian <fadi.serapian@gmail.com>
2020-03-04 21:25:26 -0500

    add play, pause and stop functionality

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+
+function toggleVideoStatus() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fas fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fas fa-pause fa-2x"></i>';
+    }
+}
+
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
+
+video.addEventListener('click', toggleVideoStatus);
+play.addEventListener('click', toggleVideoStatus);
+play.addEventListener('click', updatePlayIcon);
+video.addEventListener('click', updatePlayIcon);
+stop.addEventListener('click', stopVideo);
